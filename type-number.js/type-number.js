38460566815d9e37316b44ab4ba3e26f664f1f44
scorpion <126881227+jujuCEEPfrontend@users.noreply.github.com>
2023-09-13 09:50:05 -0300

    Add files via upload

---
+++ type-number.js/type-number.js
@@ -0,0 +1,20 @@
+// tipo number 
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operaçaoMatematica = primeironumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante 
+
+const numeroPontoFlutante = 3.3;
+const pontoFlutuateSemZero = .5;
+
+const novaOperacao = primeiroNumero /
+numeroPontoFlutuante;
+
+console.log(novaOperacao)
